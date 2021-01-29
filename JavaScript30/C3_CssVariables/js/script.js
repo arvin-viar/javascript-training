26f9093010b49ae51a328e39385fe3aadacd6a7c
ARVIN VIAR <arvinviar@ARVIN-MBP16.local>
2021-01-29 13:37:42 +0800

    🎨 Update code after watching wesbos tutorial

--- JavaScript30/C3_CssVariables/js/script.js
+++ JavaScript30/C3_CssVariables/js/script.js
@@ -1,20 +1,10 @@
-console.log('it works!');
+// Updated JavaScript
 
-function inputChange (event) {
-    if (event.target.name === 'inp-spacing') {
-        img.style.transform = `translate(${event.target.value}px,${event.target.value}px)`;
-    } else if (event.target.name === 'inp-blur') {
-        img.style.filter = `blur(${event.target.value}px)`;
-    } else if (event.target.name === 'inp-color') {
-        imgWrap.style.backgroundColor = event.target.value;
-    } else {
-        console.log('Do nothing');
-        return;
-    }
+function inputChange () {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-const img = document.querySelector('img');
-const imgWrap = document.querySelector('.css-var__content');
-
 const inputs = document.querySelectorAll('input');
-inputs.forEach(inp => inp.addEventListener('change', inputChange));
+inputs.forEach(input => input.addEventListener('change', inputChange));
+inputs.forEach(input => input.addEventListener('mousemove', inputChange));
