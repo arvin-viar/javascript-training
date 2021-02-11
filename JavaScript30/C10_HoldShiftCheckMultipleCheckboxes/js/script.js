7bf153ec785719921937233cf437ab55a8c1f3e3
ARVIN VIAR <arvinviar@ARVIN-MBP16.local>
2021-02-11 11:02:19 +0800

    🎨 Update script after watching tutorial

--- JavaScript30/C10_HoldShiftCheckMultipleCheckboxes/js/script.js
+++ JavaScript30/C10_HoldShiftCheckMultipleCheckboxes/js/script.js
@@ -1,56 +1,19 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable no-console */
-console.log('it works');
+const checkboxes = document.querySelectorAll('.chkboxes__list input[type="checkbox"]');
 
-// const checkboxContainer = document.querySelector('.chkboxes');
-const items = document.querySelectorAll('.chkboxes__list li');
-console.log(items);
-
-let isShiftPressed = false;
-let indexFrom = null;
-let indexTo = null;
-
-function checkMultiple(start, end) {
-  items.forEach((item, idx) => {
-    const checkbox = item.querySelector('input[type="checkbox"]');
-    if (start < end) {
-      if (idx >= start && idx <= end) {
-        checkbox.checked = true;
-      }
-    }
-    if (start > end) {
-      if (idx <= start && idx >= end) {
-        checkbox.checked = true;
-      }
-    }
-  });
-}
-
+let lastCheckbox;
 function checkBoxClicked(e) {
-  if (!isShiftPressed) return;
-  const itemsArray = Array.from(items);
-  if (indexFrom === null) {
-    indexFrom = itemsArray.indexOf(e.target.parentElement);
-  } else {
-    indexTo = itemsArray.indexOf(e.target.parentElement);
-    checkMultiple(indexFrom, indexTo);
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastCheckbox) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
   }
+  lastCheckbox = this;
 }
 
-function keyPressedDown(e) {
-  if (e.key === 'Shift') {
-    isShiftPressed = true;
-  }
-}
-
-items.forEach((item) => {
-  const checkbox = item.querySelector('input[type="checkbox"]');
-  checkbox.addEventListener('click', checkBoxClicked);
-});
-
-document.addEventListener('keydown', keyPressedDown);
-document.addEventListener('keyup', () => {
-  isShiftPressed = false;
-  indexFrom = null;
-  indexTo = null;
-});
+checkboxes.forEach((checkbox) => checkbox.addEventListener('click', checkBoxClicked));
