4d4bae7263a8f5179c1648880bf7d4bdcf639d53
Arvin Viar <arvin.viar@codeandtheory.com>
2021-03-05 13:59:47 +0800

    🎨 Challenge 15: Update code

--- JavaScript30/C15_LocalStorage/js/script-initial.js
+++ JavaScript30/C15_LocalStorage/js/script-initial.js
@@ -1,3 +1,39 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 console.log('it works');
+
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+const storedData = JSON.parse(localStorage.getItem('localItems')) || [];
+console.log(storedData);
+
+function renderItems(plates = [], container) {
+  container.innerHTML = plates
+    .map(
+      (plate, i) => `<li>
+        <input type="checkbox" data-idx=${i} id="item${i}" ${plate.status ? 'checked' : ''}/>
+        <label for="item${i}">${plate.name}</label>
+      </li>
+    `
+    )
+    .join('');
+  console.log(container);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const itemName = e.target.querySelector('[name="item"]').value;
+  const item = {
+    name: itemName,
+    status: true,
+  };
+  items.push(item);
+  renderItems(items, itemsList);
+  localStorage.setItem('localItems', JSON.stringify(items));
+  e.target.reset();
+}
+
+renderItems(storedData, itemsList);
+
+addItems.addEventListener('submit', handleSubmit);
--- JavaScript30/C15_LocalStorage/js/script.js
+++ JavaScript30/C15_LocalStorage/js/script.js
@@ -4,9 +4,10 @@ console.log('it works');
 
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
-const storedData = JSON.parse(localStorage.getItem('localItems')) || [];
-console.log(storedData);
+const btnCheckAll = document.querySelector('.btn-checkall');
+const btnUncheckAll = document.querySelector('.btn-uncheckall');
+const btnClear = document.querySelector('.btn-clear');
+const items = JSON.parse(localStorage.getItem('localItems')) || [];
 
 function renderItems(plates = [], container) {
   container.innerHTML = plates
@@ -18,22 +19,63 @@ function renderItems(plates = [], container) {
     `
     )
     .join('');
-  console.log(container);
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  const itemName = e.target.querySelector('[name="item"]').value;
+  const itemName = this.querySelector('[name="item"]').value;
   const item = {
     name: itemName,
-    status: true,
+    status: false,
   };
   items.push(item);
   renderItems(items, itemsList);
   localStorage.setItem('localItems', JSON.stringify(items));
-  e.target.reset();
+  this.reset();
 }
 
-renderItems(storedData, itemsList);
+function toggleChkbox(e) {
+  // eslint-disable-next-line no-useless-return
+  if (!e.target.matches('input')) return;
+
+  const el = e.target;
+  const index = el.dataset.idx;
+  items[index].status = !items[index].status;
+  localStorage.setItem('localItems', JSON.stringify(items));
+  renderItems(items, itemsList);
+}
 
 addItems.addEventListener('submit', handleSubmit);
+itemsList.addEventListener('click', toggleChkbox);
+
+function clearArray(arr) {
+  return (arr.length = 0);
+}
+
+function toggleCheck(e) {
+  const isChecked = e.target.dataset.ischecked === 'true';
+  const newItems = items.map((item) => ({
+    name: item.name,
+    status: isChecked,
+  }));
+  clearArray(items);
+  newItems.forEach((data) => {
+    items.push(data);
+  });
+  renderItems(items, itemsList);
+  localStorage.setItem('localItems', JSON.stringify(newItems));
+}
+
+function clearList() {
+  console.log('Clear List');
+  clearArray(items);
+  renderItems(items, itemsList);
+  localStorage.setItem('localItems', JSON.stringify(items));
+}
+
+// Wesbos Challenge
+btnCheckAll.addEventListener('click', toggleCheck);
+btnUncheckAll.addEventListener('click', toggleCheck);
+btnClear.addEventListener('click', clearList);
+
+renderItems(items, itemsList);
