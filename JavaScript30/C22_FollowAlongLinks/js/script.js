e3ca54f08ea4321ebac246f2a479a7650ffe4909
Arvin Viar <arvin.viar@codeandtheory.com>
2021-03-09 15:04:31 +0800

    🎨 Challenge 22 Update code

--- JavaScript30/C22_FollowAlongLinks/js/script.js
+++ JavaScript30/C22_FollowAlongLinks/js/script.js
@@ -2,14 +2,14 @@
 /* eslint-disable no-console */
 console.log('it works');
 
-const links = document.querySelectorAll('.menu a');
+const links = document.querySelectorAll('a');
 const overlay = document.querySelector('div.highlight');
 
 function applyOverlay() {
   const coordinates = this.getBoundingClientRect();
   overlay.style.width = `${coordinates.width}px`;
   overlay.style.height = `${coordinates.height}px`;
-  overlay.style.transform = `translate(${coordinates.left}px, ${coordinates.top}px`;
+  overlay.style.transform = `translate(${coordinates.left + window.scrollX}px, ${coordinates.top + window.scrollY}px`;
 }
 
 links.forEach((a) => a.addEventListener('mouseover', applyOverlay));
