0aa5a020f2b109687f10d267649cc235c651d45d
Arvin Viar <arvin.viar@codeandtheory.com>
2021-03-10 17:24:49 +0800

    ✨ Challenge 26 Stripe Follow Along Dropdown

---
+++ JavaScript30/C26_StripeFollowAlongDropdown/js/script.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
+console.log('it works');
+
+const links = Array.from(document.querySelectorAll('ul.cool > li'));
+const bgOverlay = document.querySelector('.dropdownBackground');
+
+function displayDropdown() {
+  this.classList.add('trigger-enter', 'trigger-enter-active');
+
+  const coordinates = this.getBoundingClientRect();
+  const dropdown = this.querySelector('.dropdown');
+  const ddCoords = dropdown.getBoundingClientRect();
+
+  bgOverlay.style.width = `${ddCoords.width}px`;
+  bgOverlay.style.height = `${ddCoords.height}px`;
+  bgOverlay.style.transform = `translate(${ddCoords.left + window.scrollX}px, ${coordinates.height}px`;
+  bgOverlay.classList.add('open');
+}
+
+function hideDropdown() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  bgOverlay.classList.remove('open');
+}
+
+links.forEach((link) => link.addEventListener('mouseenter', displayDropdown));
+links.forEach((link) => link.addEventListener('mouseleave', hideDropdown));
